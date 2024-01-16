adfaf4dd8f4d8c326723ee0571bdef5319180761
Caulm <40651411+Caulm@users.noreply.github.com>
2024-01-16 13:51:55 +0800

    局部稳定计算功能完成。更新示意图

--- source/ShearLagEffect/ViewModels/LocalStabilityViewModel.cs
+++ source/ShearLagEffect/ViewModels/LocalStabilityViewModel.cs
@@ -16,12 +16,16 @@ public partial class LocalStabilityViewModel : ViewModelBase
     //Bindings
     [ObservableProperty]
     private LocalStability? dbEntity;
-    [ObservableProperty]
-    private string param;
-    //加筋类型
+    //加劲类型
     public IEnumerable<StiffenerTypeDisplayItem> StiffenerTypeValues { get; }
+    [ObservableProperty]
+    private Visibility panelVisibility0;
+    [ObservableProperty]
+    private Visibility panelVisibility1;
 
     //Commands
+    //切换纵向加劲类型
+    public ICommand StiffenerTypeSelectionChangedCommand { get; }
     //计算
     public ICommand CalculateCommand { get; }
     //清空
@@ -32,10 +36,12 @@ public partial class LocalStabilityViewModel : ViewModelBase
     {
         if (parameter is not null && parameter.ContainsKey("Id") && parameter["Id"] is Int64 id)
             dbEntity = document?.LocalStabilities.Find(id);
-        param = "VAL";
         StiffenerTypeValues = Enum.GetValues(typeof(StiffenerType))
             .Cast<StiffenerType>()
             .Select(value => new StiffenerTypeDisplayItem(value));
+        PanelVisibility0 = (dbEntity.LongitudinalStiffenerType == StiffenerType.IShape) ? Visibility.Visible : Visibility.Collapsed;
+        PanelVisibility1 = (dbEntity.LongitudinalStiffenerType == StiffenerType.UShape) ? Visibility.Visible : Visibility.Collapsed;
+        StiffenerTypeSelectionChangedCommand = new RelayCommand(StiffenerTypeSelectionChanged);
         ClearAllCommand = new RelayCommand(ClearAll);
         CalculateCommand = new RelayCommand(Calculate);
     }
@@ -43,7 +49,12 @@ public partial class LocalStabilityViewModel : ViewModelBase
     public override void Refresh()
     {
         OnPropertyChanged(nameof(DbEntity));
+    }
 
+    private void StiffenerTypeSelectionChanged()
+    {
+        PanelVisibility0 = (dbEntity.LongitudinalStiffenerType == StiffenerType.IShape) ? Visibility.Visible : Visibility.Collapsed;
+        PanelVisibility1 = (dbEntity.LongitudinalStiffenerType == StiffenerType.UShape) ? Visibility.Visible : Visibility.Collapsed;
     }
 
     private void Calculate()
